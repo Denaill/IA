5fd2d0d48076541bdc084bea5251271fe0cc977f
Denaill <32781289+Denaill@users.noreply.github.com>
2018-04-15 07:39:09 -0500

    Add files via upload

---
+++ instancias/instancias/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class InstanciasItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    # comment = scrapy.Field()
+    # typee = scrapy.Field()
+    # dimension = scrapy.Field()
+    # tour_section = scrapy.Field()
+
---
+++ instancias/instancias/spiders/spider.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule, Spider
+from scrapy.linkextractors import LinkExtractor
+from instancias.items import InstanciasItem
+from scrapy.exceptions import CloseSpider
+
+class InstanciaSpider(CrawlSpider):
+    name = 'InstanciasTSP'
+    allowed_domain = ['www.elib.zib.de']
+    start_urls = ['http://elib.zib.de/pub/mp-testdata/tsp/tsplib/tsp/'] 
+
+    rules = {
+        Rule(LinkExtractor(allow = (), restrict_xpaths = ('//ul/li/text()'))),
+        Rule(LinkExtractor(allow = (), restrict_xpaths = ('//ul/li/a[@href]')), 
+                            callback = 'parse_item', follow= False)
+    }
+
+    def parse_item(self, response):
+        in_item = InstanciasItem()
+        count = 0
+        #Con la informacion de in_item se importaran todos los campos
+        in_item['name'] = response.xpath('//html/body/pre/text()').extract()
+        yield in_item
